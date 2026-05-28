afe141e614e9a6112dac0c97fe0abad7cd0c223c
Jhohan <you@example.comjhohanmiyamur@gmail.com>
2026-05-28 01:59:30 -0500

    Dashboard administrativo refinado con diseno premium

--- Fam. Guadalupe/Guadalupe.py
+++ Fam. Guadalupe/Guadalupe.py
@@ -412,7 +412,6 @@ else:
 
         st.markdown("<br>", unsafe_allow_html=True)
         st.markdown("<h3 class='titulo-principal'>CATÁLOGO DE PRODUCTOS DISPONIBLES</h3>", unsafe_allow_html=True)
-        st.markdown("<br><p style='text-align: left; font-size: 20px; margin-top: -54px; font-weight: bold; color: #d4af37;'>Bienvenidos al stock de productos disponibles y sus precios🔥</p>", unsafe_allow_html=True)
         st.markdown("<br>", unsafe_allow_html=True)
         
         # 1. BOTÓN PRINCIPAL DE ACCIÓN
