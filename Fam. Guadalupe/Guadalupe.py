65cbce213f918d53b69a960968e004379f05d699
Jhohan <you@example.comjhohanmiyamur@gmail.com>
2026-05-28 17:41:10 -0500

    Dashboard administrativo refinado con diseno premium

--- Fam. Guadalupe/Guadalupe.py
+++ Fam. Guadalupe/Guadalupe.py
@@ -126,6 +126,7 @@ URL_VIDEO_PC = "https://githubusercontent.com"
 URL_VIDEO_MOVIL = "https://githubusercontent.com"
 URL_VIDEO_LOGO = "https://githubusercontent.com"
 
+
 if os.path.exists(RUTA_CSS):
     try:
         with open(RUTA_CSS, "r", encoding="utf-8") as f:
